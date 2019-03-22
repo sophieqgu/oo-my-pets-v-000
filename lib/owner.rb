ae453e4fcd1619c0650825588b128b82eedfdea6
Sophie Gu <sophieqgu@gmail.com>
2019-03-22 16:46:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,7 @@ class Owner
     @pets[:dogs].each {|dog| dog.mood = "nervous"}
     @pets[:cats].each {|cat| cat.mood = "nervous"}
     @pets[:fishes].each {|fish| fish.mood = "nervous"}
-    @pets.foreach {|pet| pet.clear}
+    @pets.values.clear
   end 
   
   def list_pets 
